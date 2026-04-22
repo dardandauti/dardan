5e287964df1590eac44eb24e532812898fa38d2f
dardan <dardandauti@live.se>
2026-04-22 20:22:30 +0200

    123456

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/dardan/",
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 });
